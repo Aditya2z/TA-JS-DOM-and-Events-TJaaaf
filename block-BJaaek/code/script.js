bff35a81342bd3801ecbf267fe6b1944968f2d34
Aditya Dhanraj <pandeydhanrajaditya@gmail.com>
2023-02-25 12:17:57 +0530

    Both Events written

---
+++ block-BJaaek/code/script.js
@@ -0,0 +1,73 @@
+const colors = [
+  "AliceBlue",
+  "AntiqueWhite",
+  "Aqua",
+  "Aquamarine",
+  "Azure",
+  "Beige",
+  "Bisque",
+  "Black",
+  "BlanchedAlmond",
+  "Blue",
+  "BlueViolet",
+  "Brown",
+  "BurlyWood",
+  "CadetBlue",
+  "Chartreuse",
+  "Chocolate",
+  "Coral",
+  "CornflowerBlue",
+  "Cornsilk",
+  "Crimson",
+  "Cyan",
+  "DarkBlue",
+  "DarkCyan",
+  "DarkGoldenRod",
+  "DarkGray",
+  "DarkGrey",
+  "DarkGreen",
+  "DarkKhaki",
+  "DarkMagenta",
+  "DarkOliveGreen",
+  "DarkOrange",
+  "DarkOrchid",
+  "DarkRed",
+  "DarkSalmon",
+  "DarkSeaGreen",
+  "DarkSlateBlue",
+  "DarkSlateGray",
+  "DarkSlateGrey",
+  "DarkTurquoise",
+  "DarkViolet",
+  "DeepPink",
+  "DeepSkyBlue",
+  "DimGray",
+  "DimGrey",
+  "DodgerBlue",
+  "FireBrick",
+  "FloralWhite",
+  "ForestGreen",
+  "Fuchsia",
+  "Gainsboro"
+];
+
+let box1 = document.querySelector(".first");
+let i = 0;
+
+
+let btn1 = document.querySelector(".btn1");
+btn1.addEventListener('click', function() {
+    i++;
+    box1.style.backgroundColor = colors[i];
+    if(i > 49) i = 0;
+});
+
+let box2 = document.querySelector(".second");
+
+
+let btn2 = document.querySelector(".btn2");
+btn2.addEventListener('mousemove', function() {
+    i++;
+    box2.style.backgroundColor = colors[i];
+    if(i > 49) i = 0;
+});
